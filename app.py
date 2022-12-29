5f13176f3f67ab04f9fa9ded4d37c25869d1cd64
fasil shafi <zargerfasil123@gmail.com>
2022-12-29 15:51:25 +0530

    folder change

--- app.py
+++ app.py
@@ -3,17 +3,21 @@ from flask import Flask,jsonify,render_template
 
 app = Flask(__name__)
 
+
+#Home
 @app.route("/",methods=['GET','POST'])
 def home():
    return render_template('home.html')
-
+#Emoji
 @app.route("/emoji",methods=['GET','POST'])
 def emoji():
    return render_template('Emoji/emoji.html')
 
-@app.route("/last",methods=['GET','POST'])
-def last():
-   return render_template('last.html')
+#Emoji
+@app.route("/emojrecog",methods=['GET','POST'])
+def emojrecog():
+   return render_template('EmojRecog/EmojRecog.html')
+
 
 if __name__ == "__main__":
     app.run(debug = True)
