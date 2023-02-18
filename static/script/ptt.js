c35f1e66adc6c07e50ab5bfcf0be3d09c583d494
Haseeb Hijazi khan <79097096+HaseebH-Khan@users.noreply.github.com>
2023-02-18 22:01:49 +0530

    PTT updated

--- static/script/ptt.js
+++ static/script/ptt.js
@@ -1,13 +1,27 @@
-const iterations = 5; // To be decided
+const pulses = 5; // TBD
 const clickTime  = 5; // Given 5  sec
-const restTime   = 5; // Given 10 sec
+const duration   = 2; // TBD
+const restTime   = 10; // Given 10 sec
 
 const box1    = document.getElementById('box1');
 const box2    = document.getElementById('box2');
 const timer   = document.getElementById('timer');
 const result  = document.getElementById('result');
+const score  = document.getElementById('score');
 const grid    = document.getElementById('grid');
+const timerbox = document.getElementById('timer-box');
+const pul = document.getElementById('pul');
+const cor = document.getElementById('cor');
+const wrn = document.getElementById('wrn');
 
+let greyClicks = 0;
+let timeStampGray = [];
+let correctClicks = 0;
+let timeStampCorrect = [];
+let wrongClicks = 0;
+let timeStampWrong = [];
+let successArray = [];
+let isClicked = false;
 document.getElementById('proceed').addEventListener('click', function () {
   box1.style.display = 'flex';
   box2.style.display = 'flex';
@@ -15,114 +29,88 @@ document.getElementById('proceed').addEventListener('click', function () {
   document.getElementById('prompt').style.display = 'none';
 });
 
+
+function handleClicks() {
+  isClicked = true;
+  correctClicks++;
+  let time = Date.now();
+  timeStampCorrect.push(time);
+}
+
+function penaliseClicks() {
+  wrongClicks++;
+  let time = Date.now();
+  timeStampWrong.push(time);
+}
 function Game() {
-  let clickedBlue = false;
-  let clickedRed  = false;
-  let totalClicks = 0;
-  let boolIndex   = 0;
-  const clickResults = new Array(iterations).fill(0);
-
-  document.addEventListener('click', function (event) {
-    totalClicks++;
-    if(event.target.style.backgroundColor === 'blue') {
-      clickedBlue = true;
-      clickedRed  = false;
-    }
-    else if(event.target.style.backgroundColor === 'red') {
-      clickedRed  = true;
-      clickedBlue = false;
-    }
-  });
-
-  function rep() {
-    let countDown = clickTime;
-    if(Math.random() < 0.5) {
-      box2.style.backgroundColor = 'red';
-      box1.style.backgroundColor = 'blue';
-      const blueButton = box1;
-    } else {
-      box1.style.backgroundColor = 'red';
-      box2.style.backgroundColor = 'blue';
-      const blueButton = box2;
-    }
-    timer.textContent = countDown;
-    const interval = setInterval(() => {
-      countDown--;
-      timer.textContent = countDown;
-      if(countDown === 0) {
-        clearInterval(interval);
-        box1.style.backgroundColor = 'gray';
-        box2.style.backgroundColor = 'gray';
-        timer.textContent = 'STOP!';
-      }
-    }, 1000);
-
-    // try crypto.getRandomValues(new Uint32Array(1))[0] % 2
-    // use try ... catch
+  for(let t=clickTime*pulses; t>=0; t--) {
     setTimeout(() => {
-      clearInterval(interval);
-      if(Math.random() < 0.5) {
-        box2.style.backgroundColor = 'red';
-        box1.style.backgroundColor = 'blue';
-      } else {
-        box1.style.backgroundColor = 'red';
-        box2.style.backgroundColor = 'blue';
-      }
-      timer.textContent = '';
-    }, (clickTime+restTime)*1000);
+        timer.innerText = t;
+    }, ((clickTime*pulses)-t)*1000);
   }
 
-  rep();
-  const repInterval = setInterval(rep, (clickTime+restTime)*1000);
-
-  const clickInterval = setInterval(function() {
-    if(clickedBlue) {
-      clickResults[boolIndex] = 1;
-    } 
-    boolIndex++;
-    clickedBlue = false;
-  }, (clickTime+restTime)*1000);
+  for(let i = 0; i < pulses; i++) {
+    const startTime = Math.floor(Math.random() * ((clickTime-duration)*1000));
+    box2.addEventListener('click', penaliseClicks);
+    setTimeout(() => {
+      box2.removeEventListener('click', penaliseClicks);
+      box2.addEventListener('click', handleClicks);
+      box1.classList.add('animate');
+      box1.innerText = 'Pulse';
+      setTimeout(() => {
+        box2.removeEventListener('click', handleClicks);
+        box2.addEventListener('click', penaliseClicks);
+        if(isClicked) {
+          successArray.push(1);
+        } else {
+          successArray.push(0);
+        }
+        isClicked = false;
+        box1.classList.remove('animate');
+        box1.innerText = '';
+        if(isClicked) {
+          console.log('penalise');
+        }
+      }, duration*1000);
+    }, startTime + i*clickTime*1000);
+  }
 
   setTimeout(() => {
-    clearInterval(repInterval);
-  }, (iterations-1)*(clickTime+restTime)*1000);
+    box2.addEventListener('click', () => {
+      greyClicks++;
+      let time = Date.now();
+      timeStampGray.push(time);
+    });
+    box1.style.backgroundColor = 'gray';
+    box2.style.backgroundColor = 'gray';
+    box2.innerHTML = '';
+    timer.innerText = 'STOP!';
+  }, pulses*clickTime*1000);
+
+  
 
   setTimeout(() => {
-    clearInterval(clickInterval);
-    // clear screen of game elements
-    box1.style.display  = 'none';
-    box2.style.display  = 'none';
-    timer.style.display = 'none';
-    let guide = document.getElementById('guide');
-    guide.style.display = 'none';
-    guide.style.height  = '0vh';
-    document.getElementById('timer-box').style.display = 'none';
-    document.getElementById('timer-box').style.height  = '0vh';
-    grid.style.height   = '0vh';
-    // display results
     result.style.display = 'flex';
+    score.style.display = 'flex';
     let router = document.getElementById('container');
     router.style.display = 'flex';
-    const resultTable = `
-      <table>
-        <tr>
-          <th>Result</th>
-          <th></th>
-        </tr>
-        <tr>
-          <td>Total Clicks</td>
-          <td>${totalClicks-1}</td>
-        </tr>
-        <tr>
-          <td>Correct Clicks</td>
-          <td>${clickResults.reduce((a, b) => a + b, 0)}</td>
-        </tr>
-        <tr>
-          <td>Iterations</td>
-          <td>${iterations}</td>
-        </tr>
-      </table>
-      `;
-    result.innerHTML = resultTable;
-  }, (iterations)*(clickTime+restTime)*1000);
+    pul.innerText = pulses;
+    cor.innerText = successArray.reduce((a, b) => a + b, 0);
+    wrn.innerText = wrongClicks;
+    console.log('Gray Clicks');
+    console.log(greyClicks);
+    console.log(timeStampGray);
+    console.log('Correct Clicks');
+    console.log(correctClicks);
+    console.log(timeStampCorrect);
+    console.log('Wrong Clicks');
+    console.log(wrongClicks);
+    console.log(timeStampWrong);
+    console.log('Success Array');
+    console.log(successArray);
+    box1.style.display = 'none';
+    box2.style.display = 'none';
+    grid.style.display = 'none';
+    timerbox.style.display = 'none';
+  }, (pulses*clickTime+restTime)*1000);
 }
