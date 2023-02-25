8de68222ecc077ffbb16401abeede517cd326ca4
Fazeel Jaffer <fazeeljaffer@gmail.com>
2023-02-25 20:40:58 +0530

    kfdazxg

--- static/ACE/attention/attention4.js
+++ static/ACE/attention/attention4.js
@@ -46,6 +46,18 @@ while (k <= 5) {
     k++;
 }
 tot = answers2.length;
+//New Element ADDed------>
+db=[];
+dbans=[];
+i=0;
+function selectfun(ele) {
+    db.push(ele.innerText);
+}
+for(var i=0;i<db.length;i++){
+    if(i%2===0){
+        dbans.push(db[i]);
+    }
+}
 function getCheckedValue(radioName) {
     var radios = document.getElementsByName(radioName);
     for (var y = 0; y < radios.length; y++)
